1a90869f13d13fc395c4a763cd6592b9cbbabee8
방세현 <tpwnstpgus1@naver.com>
2026-02-19 15:06:12 +0900

    [FEAT] Header 생성 완료

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -6,13 +6,15 @@ export default function Header() {
         <div className="w-[70%] flex justify-between p-8 items-center">
             <div className="flex-col">
                 <p className="font-bold tracking-widest text-sm pb-1 flex">
-                    <LayoutDashboard size={20} className="text-blue-500 mr-2" />MINE-LOG</p>
+                    <LayoutDashboard size={20} className="text-blue-500 mr-2" />
+                    MINE-LOG
+                </p>
                 <h1 className="text-5xl font-black tracking-tight text-slate-900 ">
-              오늘을 <br />
-              <span className="text-blue-600">기록해보세요!</span>
-            </h1>
+                    오늘을 <br />
+                    <span className="text-blue-600">기록해보세요!</span>
+                </h1>
             </div>
             <LiveInfo />
         </div>
-    )
+    );
 }
--- components/layout/header/LiveInfo.tsx
+++ components/layout/header/LiveInfo.tsx
@@ -1,33 +1,51 @@
 'use client';
-import { useState, useEffect } from "react";
-import { Calendar, Clock, Cloud } from 'lucide-react';
+import { useState, useEffect } from 'react';
+import { Calendar, Clock, Sunrise, Sun, Coffee, Moon } from 'lucide-react';
 import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
-export default function LiveInfo () {
+export default function LiveInfo() {
     const [time, setTime] = useState(new Date());
+    const greeting =
+        time.getHours() < 6
+            ? '부지런하시네요!'
+            : time.getHours() < 12
+              ? '좋은 아침이에요!'
+              : time.getHours() < 18
+                ? '오후도 화이팅!'
+                : '편안함 밤 되세요.';
 
     useEffect(() => {
         // 1초 간격으로 시간 갱신
-        const timer = setInterval(()=> setTime(new Date()), 1000);
+        const timer = setInterval(() => setTime(new Date()), 1000);
 
         return () => clearInterval(timer);
-    },[]);
+    }, []);
 
-    return (<div className="flex flex-col items-start md:items-end gap-2 bg-white/40 backdrop-blur-sm p-4 rounded-3xl border border-white/60 shadow-sm">
-        <div className="flex items-center gap-2 text-slate-600 font-bold">
-              <Calendar size={18} />
-              <span>{format(time, 'yyyy년 MM월 dd일 (eeee)', { locale: ko })}</span>
-        </div>
-        <div className="flex items-center gap-4">
-              <div className="flex items-center gap-2 text-3xl font-black text-slate-800 tabular-nums">
-                <Clock size={24} className="text-blue-500 mt-1" />
-                {format(time, 'HH:mm:ss')}
-              </div>
-              <div className="h-8 w-[1px] bg-slate-200" />
-              <div className="flex items-center gap-2 text-slate-600">
-                <Cloud size={20} className="text-blue-300" />
-                <span className="font-bold">24°C 서울</span>
-              </div>
+    return (
+        <div className="flex flex-col items-start md:items-end gap-2 bg-white/40 backdrop-blur-sm p-4 rounded-3xl border border-white/60 shadow-sm">
+            <div className="flex items-center gap-2 text-slate-600 font-bold">
+                <Calendar size={18} />
+                <span>{format(time, 'yyyy년 MM월 dd일 (eeee)', { locale: ko })}</span>
             </div>
-    </div>)
+            <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2 text-3xl font-black text-slate-800 tabular-nums">
+                    <Clock size={24} className="text-blue-500 mt-1" />
+                    {format(time, 'HH:mm:ss')}
+                </div>
+                <div className="h-8 w-[1px] bg-slate-300" />
+                <div className="flex items-center gap-2 text-slate-600">
+                    {time.getHours() < 6 ? (
+                        <Sunrise size={20} className="text-amber-500" />
+                    ) : time.getHours() < 12 ? (
+                        <Sun size={20} className="text-yellow-500" />
+                    ) : time.getHours() < 18 ? (
+                        <Coffee size={20} className="text-orange-500" />
+                    ) : (
+                        <Moon size={20} className="text-indigo-400" />
+                    )}
+                    <span className="font-bold">{greeting}</span>
+                </div>
+            </div>
+        </div>
+    );
 }
