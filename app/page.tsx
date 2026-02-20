d8e705777e551ea9ac29f25077e3afc9c4027dcb
방세현 <tpwnstpgus1@naver.com>
2026-02-20 18:17:43 +0900

    [CHORE] motion/react 라이브러리 설치 & todolist 영역 height 지정

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import TodoSection from '@/components/TodoSection';
 export default function Home() {
     return (
         <div className="w-[70%]">
-            <div className="flex w-[100%] mt-8 justify-between">
-                <TodoSection />
-                <MemoSection />
+            <div className=" w-[100%] flex w-[100%] mt-8 justify-between">
+                <div className="w-[60%] h-[450px]">
+                    <TodoSection />
+                </div>
             </div>
         </div>
     );
