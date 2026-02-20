393dbe180333620b278175b169e68f5db04fa32b
방세현 <tpwnstpgus1@naver.com>
2026-02-20 16:37:13 +0900

    [FEAT] 대시보드 화면 1행 배치

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import MemoSection from '@/components/MemoSection';
+import TodoSection from '@/components/TodoSection';
 export default function Home() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div className="w-[70%]">
+            <div className="flex w-[100%] mt-8 justify-between">
+                <TodoSection />
+                <MemoSection />
+            </div>
+        </div>
+    );
 }
